7c80b74063eeab6c46a2ea5b8ac5a41a8a2091ad
RyanWD <ryan.saunders86@icloud.com>
2024-04-02 16:38:12 +0100

    complete jobs CRUD project

--- React-jobs-project/src/pages/JobPage.jsx
+++ React-jobs-project/src/pages/JobPage.jsx
@@ -1,15 +1,28 @@
 // import { useEffect, useState } from "react"
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { toast } from "react-toastify"
 // import Spinner from "../components/Spinner";
 
-const JobPage = () => {
-  const { id } = useParams();
 
+const JobPage = ( {deleteJob} ) => {
+  const { id } = useParams();
+const navigate = useNavigate()
   // Alternative way of returning data:
   // See App.jsx routes, jobLoader is stated as a 'loader'
   const job = useLoaderData();
+
+  const onDeleteJob = (jobId) => {
+    const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+    if (!confirm) return
+
+    deleteJob(jobId)
+
+    toast.success('Job deleted successfully')
+    navigate('/jobs')
+  }
 
   // Standard way of returning data:
 
@@ -36,6 +49,8 @@ const JobPage = () => {
 
   //  fetchJob()
   // }, [])
+
+  
 
   return (
     <>
@@ -108,12 +123,12 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button onClick={() => onDeleteJob(job.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
